5e42b8963014808230c5e48b7398f846a8076e2f
inchan <inchan.kang@9folders.com>
2024-12-24 17:27:14 +0900

    [*] query 추가

--- Sources/main.swift
+++ Sources/main.swift
@@ -7,60 +7,70 @@
 
 import Foundation
 
-let query: String = "광진구 개발"
-let searchQuries = query.toSearchQuries
-let old = try XMLLoader(key: query).fromLocalFile()
-if let old = old {
-    print("old: \(old)")
-}
+let quries = [
+    "광진구 개발",
+    "중곡동 개발",
+    "중곡역 개발",
+    "중곡동 재개발",
+    "중곡역 재개발"
+]
 
-let engines = searchQuries.map { [NaverSearch(query: $0)] }.flatMap{ $0 }
-let fetched = await engines.asyncMap { engine -> Result<RssFeed, Error> in
-    do {
-        let fetched = try await engine.fetch()
-        return Result.success(fetched)
-    } catch {
-        return Result.failure(error)
+for query in quries {
+    print("query: \(query)\n")
+
+    let searchQuries = query.toSearchQuries
+    let old = try XMLLoader(key: query).fromLocalFile()
+    if let old = old {
+        print("old: \(old)\n")
     }
-}
 
-let feeds = fetched
-    .reduce([Feed]()) { partialResult, r in
-        switch r {
-        case .success(let rss): partialResult + rss.feeds
-        case .failure(_): partialResult
+    let engines = searchQuries.map { [NaverSearch(query: $0)] }.flatMap{ $0 }
+    let fetched = await engines.asyncMap { engine -> Result<RssFeed, Error> in
+        do {
+            let fetched = try await engine.fetch()
+            return Result.success(fetched)
+        } catch {
+            return Result.failure(error)
         }
     }
-    .distinct()
-    .filterSimilar()
 
-let merged = fetched
-    .compactMap { r in
-        switch r {
-        case .success(let rss): rss
-        case .failure(_): nil
+    let feeds = fetched
+        .reduce([Feed]()) { partialResult, r in
+            switch r {
+            case .success(let rss): partialResult + rss.feeds
+            case .failure(_): partialResult
+            }
+        }
+        .distinct()
+        .filterSimilar()
+
+    let merged = fetched
+        .compactMap { r in
+            switch r {
+            case .success(let rss): rss
+            case .failure(_): nil
+            }
+        }
+        .first
+        .map{ rssFeed in
+            RssFeed(
+                title: rssFeed.title,
+                desc: rssFeed.desc,
+                link: rssFeed.link,
+                updated: rssFeed.updated,
+                author: "\(RSSType.Integration)",
+                feeds: feeds,
+                type: .Integration
+            )
+        }
+
+    if let merged = merged {
+        let publisher = XMLPublisher(rssFeed: merged, key: query)
+        do {
+            try publisher.publish()
+        }
+        catch {
+            print("publish error: \(error)")
         }
     }
-    .first
-    .map{ rssFeed in
-        RssFeed(
-            title: rssFeed.title,
-            desc: rssFeed.desc,
-            link: rssFeed.link,
-            updated: rssFeed.updated,
-            author: "\(RSSType.Integration)",
-            feeds: feeds,
-            type: .Integration
-        )
-    }
-
-if let merged = merged {
-    let publisher = XMLPublisher(rssFeed: merged, key: query)
-    do {
-        try publisher.publish()
-    }
-    catch {
-        print("publish error: \(error)")
-    }
 }
-
